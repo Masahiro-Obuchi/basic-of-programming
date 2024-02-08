9375173780079bbb2ed4732cbf3321262171b32f
Masairo Obuchi <o.masahiro0328@gmail.com>
2024-02-08 23:49:28 +0900

    update sec15/quick_sort.ml (answer_15_1)

--- sec15/quick_sort.ml
+++ sec15/quick_sort.ml
@@ -32,9 +32,9 @@ let rec quick_sort lst =
 (* 目的 : lst の中からnよりpである要素のみを取り出す *)
 (* take : int -> int list -> (int -> int -> bool) -> int list *)
 let take n lst p = List.filter (fun item -> p item n) lst
-(* 目的 : lstの中からnより小さい要素のみを取り出す *)
+(* 目的 : lstの中からn以下の要素のみを取り出す *)
 (* take_less : int -> int list -> int list *)
-    in let take_less n lst = take n lst (<)
+    in let take_less n lst = take n lst (<=)
 (* 目的 : lstの中からnより大きい要素のみを取り出す *)
 (* take_greater : int -> int list -> int list *)
     in let take_greater n lst = take n lst (>)
@@ -50,3 +50,22 @@ let test2 = quick_sort [1] = [1]
 let test3 = quick_sort [1; 2] = [1; 2]
 let test4 = quick_sort [2; 1] = [1; 2]
 let test5 = quick_sort [5; 4; 9; 8; 2; 3] = [2; 3; 4; 5; 8; 9]
+
+(* answer_15_1 : 本の例そのままだと等しい数字が複数個あった際に正しい答えを返さない *)
+let test6 = quick_sort [1; 1; 1; 1; 1] = [1; 1; 1; 1; 1]
+let test7 = quick_sort [1; 2; 1; 1; 1] = [1; 1; 1; 1; 2]
+let test8 = quick_sort [1; 1; 1; 2; 1] = [1; 1; 1; 1; 2]
+let test9 = quick_sort [1; 1; 2; 1; 1] = [1; 1; 1; 1; 2]
+let test10 = quick_sort [1; 2; 1; 2; 1] = [1; 1; 1; 2; 2]
+let test11 = quick_sort [1; 2; 2; 1; 1] = [1; 1; 1; 2; 2]
+let test12 = quick_sort [2; 1; 1; 1; 1] = [1; 1; 1; 1; 2]
+let test13 = quick_sort [2; 1; 1; 1; 2] = [1; 1; 1; 2; 2]
+let test14 = quick_sort [2; 1; 1; 2; 1] = [1; 1; 1; 2; 2]
+let test15 = quick_sort [2; 1; 2; 1; 1] = [1; 1; 1; 2; 2]
+let test16 = quick_sort [2; 2; 1; 1; 1] = [1; 1; 1; 2; 2]
+let test17 = quick_sort [1; 2; 2; 2; 1] = [1; 1; 2; 2; 2]
+let test18 = quick_sort [2; 1; 2; 2; 1] = [1; 1; 2; 2; 2]
+let test19 = quick_sort [2; 2; 1; 2; 1] = [1; 1; 2; 2; 2]
+let test20 = quick_sort [2; 2; 2; 1; 1] = [1; 1; 2; 2; 2]
+
+
